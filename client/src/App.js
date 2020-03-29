983ac5018939d8885eea67c286eb1174fb22eb79
Storey Sheldon <storey.e.sheldon@gmail.com>
2020-03-29 11:29:17 -0500

    basic routing and filw structure set up

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Search from './pages/Search';
+import Saved from './pages/Saved';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        {/* <Nav /> */}
+        <Switch>
+          <Route exact path={["/", "/search"]}>
+            <Search />
+          </Route>
+          <Route exact path="/saved">
+            <Saved />
+          </Route>
+          {/* <Route>
+            <NoMatch />
+          </Route> */}
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
